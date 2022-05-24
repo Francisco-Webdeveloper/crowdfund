0e9ab1e4ce79cf3e3ab87f93aca708e93cf5ec10
Francisco Santos <francisco.santos.lx81@gmail.com>
2022-05-24 16:58:18 +0200

    made project responsive

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,19 @@
 import styles from "./Navbar.module.scss";
+import menuIcon from "../../icons/icon-hamburger.svg";
+import { useState } from "react";
 
 export const Navbar = () => {
   return (
     <div className={styles.navbar}>
       <h3 className={styles.logo}>crowdfund</h3>
+      {/* Mobile */}
+      <img
+        className={styles.burgerMenu}
+        src={menuIcon}
+        alt="menu-icon"
+        height="15px"
+      />
+      {/* Desktop */}
       <ul className={styles.links}>
         <li>About</li>
         <li>Discover</li>
--- src/components/ProjectHeader/index.js
+++ src/components/ProjectHeader/index.js
@@ -4,20 +4,28 @@ import bookmarkIcon from "../../icons/icon-bookmark.svg";
 
 export const ProjectHeader = ({ onClick }) => {
   return (
-    <div className={styles.backProject}>
+    <div className={styles.backProjectCard}>
       <img className={styles.logo} src={logo} alt="logo" />
       <h2 className={styles.title}>Mastercraft Bamboo Monitor Riser</h2>
       <p className={styles.description}>
-        A beautiful & handcrafted monitor stand to reduce neck and eye strain.
+        A beautifully handcrafted monitor stand to reduce neck and eye strain.
       </p>
       <div className={styles.buttons}>
         <button className={styles.backProjectBtn} onClick={onClick}>
           Back this project
         </button>
 
+        {/* Mobile */}
+        <img
+          className={styles.bookmarkIconMobile}
+          src={bookmarkIcon}
+          alt="bookmark"
+        />
+
+        {/* DESKTOP  */}
         <button className={styles.bookmarkBtn}>
           <img
-            className={styles.bookmarkIcon}
+            className={styles.bookmarkIconDesktop}
             src={bookmarkIcon}
             alt="bookmark"
           />
