3da50096bec48fb76e2556ebeed7375dbd25c72b
Francisco Santos <francisco.santos.lx81@gmail.com>
2022-05-27 21:07:21 +0200

    added/updated data

---
+++ src/campaignsData.js
@@ -0,0 +1,28 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  data: {
+    campaigns: [
+      {
+        product: "Bamboo Stand",
+        pledgeAmount: 25,
+        description:
+          " You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and you’ll be added to a special Backer member list.",
+        stock: 101,
+      },
+      {
+        product: "Black Edition Stand",
+        pledgeAmount: 75,
+        description:
+          "You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer member list. Shipping is included.",
+        stock: 64,
+      },
+      {
+        product: "Mahogany Special Edition",
+        pledgeAmount: 200,
+        description:
+          "You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You’ll be added to our Backer member list. Shipping is included.",
+        stock: 0,
+      },
+    ],
+  },
+};
---
+++ src/projectsData.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  data: {
+    projects: [
+      {
+        id: "1",
+        moneyBacked: "$89,914",
+        totalBackers: "5,007",
+        daysLeft: 56,
+        progress: 89.914,
+        title: "Mastercraft Bamboo Monitor Riser",
+        description:
+          "A beautifully handcrafted monitor stand to reduce neck and eye strain.",
+      },
+      {
+        id: "2",
+        moneyBacked: "$89,914",
+        totalBackers: "5,007",
+        daysLeft: 56,
+        progress: 89.914,
+        title: "New Cool game",
+        description: "A beautifully game.",
+      },
+    ],
+  },
+};
