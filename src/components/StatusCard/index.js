fe6c719e5a3990a1ea0eafc969bf375a8f3386b2
Francisco Santos <francisco.santos.lx81@gmail.com>
2022-06-16 18:39:34 +0200

    implement countdown

--- src/components/StatusCard/index.js
+++ src/components/StatusCard/index.js
@@ -3,10 +3,10 @@ import { ProgressBar } from "react-bootstrap";
 import { CountdownTimer } from "../CountdownTimer";
 
 export const StatusCard = ({ daysLeft, projectStatus, goal }) => {
-  const SIXTY_DAYS_IN_MS = 60 * 24 * 60 * 60 * 1000;
+  const DAYS_IN_MS = daysLeft * 24 * 60 * 60 * 1000;
   const NOW_IN_MS = new Date().getTime();
 
-  const dateTimeAfterSixtyDays = NOW_IN_MS + SIXTY_DAYS_IN_MS;
+  const dateTimeAfterProjectDuration = NOW_IN_MS + DAYS_IN_MS;
 
   return (
     <div className={styles.status}>
@@ -22,7 +22,7 @@ export const StatusCard = ({ daysLeft, projectStatus, goal }) => {
         <h1>{daysLeft}</h1>
         <p>days left</p>
       </div> */}
-      <CountdownTimer targetDate={dateTimeAfterSixtyDays} />
+      <CountdownTimer targetDate={dateTimeAfterProjectDuration} />
       <ProgressBar
         now={(projectStatus.moneyBacked / goal) * 100}
         className={styles.progressBar}
