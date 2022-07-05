7305c09a7d6a927064199c312c03bf180412b696
Francisco Santos <francisco.santos.lx81@gmail.com>
2022-07-05 16:15:56 +0200

    connected project to firebase

---
+++ src/base.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBzqvUfMHomx7qBvuyfGgzKnjgQRq9QkbI",
+  authDomain: "crowdfund-7dcab.firebaseapp.com",
+  projectId: "crowdfund-7dcab",
+  storageBucket: "crowdfund-7dcab.appspot.com",
+  messagingSenderId: "1026301467656",
+  appId: "1:1026301467656:web:c75ba2d3fd550ad55405bd",
+  measurementId: "G-LZ7M1903G3",
+};
+
+initializeApp(firebaseConfig);
